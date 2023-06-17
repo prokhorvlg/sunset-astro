f2785d82faff03164cfc06625c9490b4bab8c770
Valentin Sigalov <vlgstudent036@gmail.com>
2023-06-16 21:06:35 -0400

    Stub for build a bot

---
+++ src/components/special/BuildABotForm.tsx
@@ -0,0 +1,12 @@
+const BuildABotForm = () => {
+    return (
+        <div className="build-a-bot-container">
+            <h1>Cybernetic Profile Sheet</h1>
+            <p>Provided by DYNATEK</p>
+
+            <input type="text"/>
+        </div>
+    )
+}
+
+export default BuildABotForm
