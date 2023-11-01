5869b5e8195cfcfd95d00dc58bc987fce70b359a
Valentin Sigalov <vlgstudent036@gmail.com>
2023-11-01 00:12:47 -0400

    Basically got the new map working mostly

--- src/components/special/SystemMap/SystemMapReact.tsx
+++ src/components/special/SystemMap/SystemMapReact.tsx
@@ -1,7 +1,7 @@
-import { MAP_DISTANCE_FACTOR, MAP_SCALE_FACTOR } from '@/components/special/SystemMap/data/constants';
+import { MAP_DISTANCE_FACTOR, MAP_SCALE_FACTOR, MAP_DEFAULT_COLOR } from '@/components/special/SystemMap/data/constants';
 import { locationsData } from '@/components/special/SystemMap/data/locationsData';
 import { LocationNode } from '@/components/special/SystemMap/types';
-import { findNewPoint } from '@/components/special/SystemMap/WorldGenerationHelpers';
+import { findNewPoint, increaseBrightness } from '@/components/special/SystemMap/WorldGenerationHelpers';
 import { useState } from 'react';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import './SystemMap.scss'
@@ -38,16 +38,50 @@ const SystemMapReact = () => {
     )
 };
 
+// Generates the orbit paths for each major object.
 const SystemMapOrbit = ({
-    location
+    location,
+    parent,
+    parentCoordinates,
+    zIndex
 }: {
     location: LocationNode
+    parent?: LocationNode
+    parentCoordinates?: {
+        x: number,
+        y: number
+    },
+    zIndex?: number
 }) => {
+    const objectPoint = findNewPoint(0, 0, location.startingAngle, location.distance * MAP_DISTANCE_FACTOR);
+    const zIndexCurrent = zIndex ? zIndex + 1 : 200
+
     return (
-        <></>
+        <div className="map-orbit-ring-container" style={{
+            left: parentCoordinates?.x || 0,
+            top: parentCoordinates?.y || 0,
+        }}>
+            <div className="map-orbit-ring" style={{
+                height: location.distance * MAP_DISTANCE_FACTOR * 2,
+                width: location.distance * MAP_DISTANCE_FACTOR * 2,
+                //borderColor: location.color,
+                zIndex: zIndexCurrent
+            }} />
+            {location.children?.slice(0).reverse().map((child) => {
+                return (
+                    <SystemMapOrbit 
+                        location={child} 
+                        parent={parent}
+                        parentCoordinates={objectPoint}
+                        zIndex={zIndexCurrent}
+                    />
+                )
+            })}
+        </div>
     )
 }
 
+// Generates each individual location.
 const SystemMapLocation = ({
     location,
     parent,
@@ -61,30 +95,21 @@ const SystemMapLocation = ({
     }
 }) => {
     const objectPoint = findNewPoint(0, 0, location.startingAngle, location.distance * MAP_DISTANCE_FACTOR);
-    const radius = location.radius ? location.radius * MAP_SCALE_FACTOR * 1.4 : 5
+    const radius = location.radius ? location.radius * MAP_SCALE_FACTOR * 2.2 : 5
+    const color = location.color ? increaseBrightness(location.color, 20) : MAP_DEFAULT_COLOR
 
     return (
         <>
-            {parentCoordinates &&
-                <div className="map-orbit-ring" style={{
-                    left: parentCoordinates.x,
-                    top: parentCoordinates.y,
-                    height: location.distance * MAP_DISTANCE_FACTOR,
-                    width: location.distance * MAP_DISTANCE_FACTOR,
-                    borderColor: location.color
-                }}></div>
-            }
-            <div className="map-location" style={{
+            <div className="map-location-container" style={{
                 left: objectPoint.x,
                 top: objectPoint.y,
-                height: radius,
-                width: radius,
-                backgroundColor: location.color
             }}>
-                
-                
-            </div>
-            {location.children?.map((child) => {
+                <div className="map-location" style={{
+                    height: radius,
+                    width: radius,
+                    backgroundColor: color
+                }} />
+                {location.children?.map((child) => {
                     return (
                         <SystemMapLocation 
                             location={child} 
@@ -93,6 +118,8 @@ const SystemMapLocation = ({
                         />
                     )
                 })}
+            </div>
+            
         </>
     )
 }
