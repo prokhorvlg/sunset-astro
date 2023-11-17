4dcf4674d97a2fb4f9a4eca9c19e600559d27ec1
Valentin Sigalov <vlgstudent036@gmail.com>
2023-11-17 12:53:54 -0500

    Cleanup, prep for upgrade to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-import markdownConfig from './markdown.config'
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sunset-system.com',
-  integrations: [mdx({
-  }), sitemap(), react(), image()],
+  integrations: [mdx(), sitemap(), react(), image()],
   vite: {
     define: {
       'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`
     }
-  },
-  //output: "hybrid",
-  //adapter: vercel()
+  }
 });
