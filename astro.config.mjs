808690447ba98d4730737df7c2380d6805be4171
Valentin Sigalov <vlgstudent036@gmail.com>
2023-11-18 01:21:26 -0500

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
   vite: {
     define: {
       'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`
+    },
+    ssr: {
+      noExternal: ["react-icons"],
     }
   }
 });
